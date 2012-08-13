48ce3b60fc9e24c1c70045a0007f975722f9e879
srinituraga <sturaga@gatsby.ucl.ac.uk>
2012-08-13 17:56:19 +0100

    - works for 2d AND 3d images - switched from using sq-sq loss to the hinge loss - computing fp, tp, fn, tn - can simultaneously compute pos and/or neg example pairs - can specify binarization threshold - fixed "bug" where border pixels were just ignored. borders pixels and edges are now correctly handled.

--- MalisCriterion.lua
+++ MalisCriterion.lua
@@ -1,46 +1,109 @@
 require 'nn'
 local MalisCriterion, parent = torch.class('nn.MalisCriterion', 'nn.Criterion')
 
-function MalisCriterion:__init(tree, metric, connex, margin)
+function MalisCriterion:__init(tree, metric, connex, threshold, margin, alternateposneg)
    parent.__init(self)
    connex = connex or 4
+   -- 2d connectivities
    if connex == 4 then
-      self.nhood = torch.Tensor(2, 2)
-      self.nhood[1][1] = 0  -- edge1: y offset
-      self.nhood[1][2] = 1  -- edge1: x offset
-      self.nhood[2][1] = 1  -- edge2: y offset
-      self.nhood[2][2] = 0  -- edge2: x offset
+      self.twoD = true
+      self.nhood = torch.Tensor(2, 3)
+      self.nhood[1][1] = 0  -- edge1: z offset
+      self.nhood[1][2] = 0  -- edge1: y offset
+      self.nhood[1][3] = 1  -- edge1: x offset
+      self.nhood[2][1] = 0  -- edge1: z offset
+      self.nhood[2][2] = 1  -- edge2: y offset
+      self.nhood[2][3] = 0  -- edge2: x offset
    elseif connex == 8 then
-      self.nhood = torch.Tensor(4, 2)
-      self.nhood[1][1] = 0  -- edge1: y offset
-      self.nhood[1][2] = 1  -- edge1: x offset
-      self.nhood[2][1] = 1  -- edge2: y offset
-      self.nhood[2][2] = 0  -- edge2: x offset
-      self.nhood[3][1] = 1  -- edge3: y offset
-      self.nhood[3][2] = 1  -- edge3: x offset
-      self.nhood[4][1] = 1  -- edge4: y offset
-      self.nhood[4][2] = -1 -- edge4: x offset
+      self.twoD = true
+      self.nhood = torch.Tensor(4, 3)
+      self.nhood[1][1] = 0  -- edge1: z offset
+      self.nhood[1][2] = 0  -- edge1: y offset
+      self.nhood[1][3] = 1  -- edge1: x offset
+      self.nhood[2][1] = 0  -- edge1: z offset
+      self.nhood[2][2] = 1  -- edge2: y offset
+      self.nhood[2][3] = 0  -- edge2: x offset
+      self.nhood[3][1] = 0  -- edge1: z offset
+      self.nhood[3][2] = 1  -- edge3: y offset
+      self.nhood[3][3] = 1  -- edge3: x offset
+      self.nhood[4][1] = 0  -- edge1: z offset
+      self.nhood[4][2] = 1  -- edge4: y offset
+      self.nhood[4][3] = -1 -- edge4: x offset
+   -- 3d connectivities
+   elseif connex == 6 then
+      self.twoD = false
+      self.nhood = torch.Tensor(3, 3)
+      self.nhood[1][1] = 0  -- edge1: z offset
+      self.nhood[1][2] = 0  -- edge1: y offset
+      self.nhood[1][3] = -1 -- edge1: x offset
+      self.nhood[2][1] = 0  -- edge1: z offset
+      self.nhood[2][2] = -1 -- edge2: y offset
+      self.nhood[2][3] = 0  -- edge2: x offset
+      self.nhood[3][1] = -1 -- edge1: z offset
+      self.nhood[3][2] = 0  -- edge2: y offset
+      self.nhood[3][3] = 0  -- edge2: x offset
    else
-      error('connexity must be one of: 4 | 8')
+      error('connexity must be one of 4 | 8 for 2d images and 6 for 3d images')
    end
+   self.threshold = threshold or 0.5
    self.margin = margin or 0.3
-   self.posexample = false
-   self.tree = tree or 'max'
-   self.inputtemp = torch.Tensor()
+   self.posexample = true
+   self.negexample = true
+   self.alternateposneg = alternateposneg or false
+   if self.alternateposneg then
+      self.negexample = false -- negative example first
+   end
    self.metric = metric or 'loss' -- loss | classified | rand
+   self.tree = tree or 'max'
+   if self.tree == 'min' then
+      self.threshold = -self.threshold
+   end
+   self.inputtemp = torch.Tensor()
+   self.targettemp = torch.Tensor()
 end
 
-function MalisCriterion:forward(input, target)
-   local e = {}
-   if self.tree == 'min' then
-      self.inputtemp:resizeAs(input):copy(input):mul(-1):add(1)
-      e.loss,e.classified,e.rand = input.nn.MalisCriterion_forward(self, self.inputtemp, target)
-   else
-      e.loss,e.classified,e.rand = input.nn.MalisCriterion_forward(self, input, target)
-   end
-   self.posexample = not self.posexample
+function MalisCriterion:forward(input, target, posexample, negexample)
    self._i = input
    self._t = target
+
+   -- pos? neg?
+   if self.alternateposneg then
+      self.posexample = not self.posexample
+      self.negexample = not self.negexample
+   end
+   if (posexample ~= nil) then
+      self.posexample = posexample
+   end
+   if (negexample ~= nil) then
+      self.negexample = negexample
+   end
+
+   -- ensure contiguity by copying (contiguous() seems not to work properly...? or my scoping is bad...?)
+   self.inputtemp:resizeAs(input):copy(input)
+   self.targettemp:resizeAs(target):copy(target)
+
+   -- shapes and signs
+   if self.tree == 'min' then
+      self.inputtemp:mul(-1)
+   end
+   if self.twoD then -- convert to 3d by inserting a dimension
+      self.inputtemp:resize(input:size(1),1,input:size(2),input:size(3))
+      self.targettemp:resize(1,target:size(1),target:size(2))
+   end
+
+   -- run the MST and compute the loss and its derivative
+   local e = {}
+   e.loss,e.classified,e.rand = input.nn.MalisCriterion_forward(self, self.inputtemp, self.targettemp)
+
+   -- shapes and signs
+   if self.tree == 'min' then
+      self.gradInput:mul(-1)
+   end
+   if self.twoD then
+      self.gradInput:resize(input:size())
+   end
+
+   -- return the goodness score
    self.output = e[self.metric]
    return self.output
 end
